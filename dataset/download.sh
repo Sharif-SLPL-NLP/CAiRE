a01f2eff23756fb457bf29c5ed1e2e39bcaf7d76
Ali <alistvt@gmail.com>
2022-03-24 19:40:14 +0430

    Create download.sh

---
+++ dataset/download.sh
@@ -0,0 +1,5 @@
+!wget https://doc2dial.github.io/multidoc2dial/file/multidoc2dial.zip -O multidoc2dial.zip
+!unzip multidoc2dial.zip
+
+!wget https://doc2dial.github.io/multidoc2dial/file/multidoc2dial_domain.zip -O multidoc2dial_domain.zip
+!unzip multidoc2dial_domain.zip
